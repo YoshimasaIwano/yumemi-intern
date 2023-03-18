f712924f892553602badf0cfd60aaa0a5c5f5bce
YoshimasaIwano <44yoshimasa03@gmail.com>
2023-03-18 04:09:40 -0700

    deploy it on firebase

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main';
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.VITE_API_KEY,
+  projectId: process.env.VITE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+
 function App() {
   return (
     <div className="App">
